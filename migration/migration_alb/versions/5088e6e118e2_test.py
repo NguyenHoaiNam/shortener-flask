bc46f88d0dd059a5f4eddc864831ae77545acca6
Nam Nguyen Hoai <namnh@vn.fujitsu.com>
2017-03-31 08:23:00 +0700

    Update new file

---
+++ migration/migration_alb/versions/5088e6e118e2_test.py
@@ -0,0 +1,21 @@
+"""test
+
+Revision ID: 5088e6e118e2
+Revises: 
+Create Date: 2017-03-31 08:22:37.905624
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5088e6e118e2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
